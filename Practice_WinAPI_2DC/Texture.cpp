07fb6cc92dbac3b043bcae25e892e0762bace165
MoonSangDon <moon3225@naver.com>
2022-03-07 01:08:51 +0900

    이미지 로드 실패시 Assert 발생으로 수정했습니다.

--- Practice_WinAPI_2DC/Texture.cpp
+++ Practice_WinAPI_2DC/Texture.cpp
@@ -6,7 +6,7 @@ void Texture::Load(const wstring& _strFPath)
 	m_hBit = (HBITMAP)LoadImage(nullptr, _strFPath.c_str(), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);//경로를 받고, 비트 맵 데이터를 로딩 하고, 사이즈는 0,0(Defalut사이즈), 로드옵션을 지정하고, 
 																																																	//그것을 m_hBit 핸들로 받는다.
 	if (nullptr == m_hBit)//로딩을 완료 했는데도, 비트맵 핸들값이 널값이라면, 로딩에 실패 한 것이다.(ID가 안나왔음->로딩실패)
-		return;//TODO: assert 로 막을것
+		assert(0);
 
 }
 
